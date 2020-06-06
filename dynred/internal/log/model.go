e9f1ddc52fafbf5eab23ca9c7eacfdd211b9f096
piroyoung <piroyoung@users.noreply.github.com>
2020-06-06 10:52:34 +0900

    add: tag

--- dynred/internal/log/model.go
+++ dynred/internal/log/model.go
@@ -6,25 +6,25 @@ import (
 )
 
 type Log struct {
-	id        string
-	loggedAt  int64
-	path      string
-	tag       string
-	query     string
-	referer   string
-	userAgent string
-	language  string
+	id           string
+	loggedAt     int64
+	path         string
+	trackingCode string
+	query        string
+	referer      string
+	userAgent    string
+	language     string
 }
 
 func NewLog(c *gin.Context) Log {
 	return Log{
-		id:        c.Request.Header.Get("Cookie"),
-		loggedAt:  time.Now().Unix(),
-		path:      c.Request.URL.Path,
-		tag:       c.Query("t"),
-		query:     c.Request.URL.RawQuery,
-		referer:   c.Request.Header.Get("Referer"),
-		userAgent: c.Request.Header.Get("User-Agent"),
-		language:  c.Request.Header.Get("Accept-Language"),
+		id:           c.Request.Header.Get("Cookie"),
+		loggedAt:     time.Now().Unix(),
+		path:         c.Request.URL.Path,
+		trackingCode: c.Query("t"),
+		query:        c.Request.URL.RawQuery,
+		referer:      c.Request.Header.Get("Referer"),
+		userAgent:    c.Request.Header.Get("User-Agent"),
+		language:     c.Request.Header.Get("Accept-Language"),
 	}
 }
